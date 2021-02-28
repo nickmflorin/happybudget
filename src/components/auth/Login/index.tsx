5025dc6887a8972e2d4a4afb1439567d289a69b9
nickmflorin <nickmflorin@gmail.com>
2021-02-27 21:59:53 -0500

    Improvements to login styling

--- src/components/auth/Login/index.tsx
+++ src/components/auth/Login/index.tsx
@@ -8,6 +8,9 @@ import { ClientError, NetworkError, renderFieldErrorsInForm } from "api";
 import { login } from "services";
 
 import LoginForm, { ILoginFormValues } from "./LoginForm";
+import GreenBudgetIcon from "./GreenBudgetIcon";
+
+import "./index.scss";
 
 const Login = (): JSX.Element => {
   const [loading, setLoading] = useState(false);
@@ -17,41 +20,48 @@ const Login = (): JSX.Element => {
 
   return (
     <div className={"login-page"}>
-      <Typography.Title>
-        <span className={"raleway--bold mr--10"}>{"Sign In"}</span>
-        <span className={"ibm-plex-sans--regular"}>{"Cloud based budgeting at your fingertips."}</span>
-      </Typography.Title>
-      {!isNil(globalError) && <div className={"global-error"}>{globalError}</div>}
-      <LoginForm
-        className={"mb--20 mt--20"}
-        form={form}
-        loading={loading}
-        onSubmit={(values: ILoginFormValues) => {
-          if (!isNil(values.email) && !isNil(values.password)) {
-            login(values.email.toLowerCase(), values.password)
-              .then(() => {
-                history.push("/");
-              })
-              .catch((e: Error) => {
-                if (e instanceof ClientError) {
-                  if (!isNil(e.errors.__all__)) {
-                    setGlobalError(e.errors.__all__[0].message);
-                  } else {
-                    // Render the errors for each field next to the form field.
-                    renderFieldErrorsInForm(form, e);
-                  }
-                } else if (e instanceof NetworkError) {
-                  setGlobalError("There was a problem communicating with the server.");
-                } else {
-                  throw e;
-                }
-              })
-              .finally(() => {
-                setLoading(false);
-              });
-          }
-        }}
-      />
+      <div className={"login-page-left"}>
+        <div className={"form-container"}>
+          <Typography.Title className={"title"}>{"Sign In"}</Typography.Title>
+          <Typography.Title className={"subTitle"}>{"Cloud based budgeting at your fingertips."}</Typography.Title>
+          {!isNil(globalError) && <div className={"global-error"}>{globalError}</div>}
+          <LoginForm
+            className={"mb--20 mt--20"}
+            form={form}
+            loading={loading}
+            onSubmit={(values: ILoginFormValues) => {
+              if (!isNil(values.email) && !isNil(values.password)) {
+                login(values.email.toLowerCase(), values.password)
+                  .then(() => {
+                    history.push("/");
+                  })
+                  .catch((e: Error) => {
+                    if (e instanceof ClientError) {
+                      if (!isNil(e.errors.__all__)) {
+                        setGlobalError(e.errors.__all__[0].message);
+                      } else {
+                        // Render the errors for each field next to the form field.
+                        renderFieldErrorsInForm(form, e);
+                      }
+                    } else if (e instanceof NetworkError) {
+                      setGlobalError("There was a problem communicating with the server.");
+                    } else {
+                      throw e;
+                    }
+                  })
+                  .finally(() => {
+                    setLoading(false);
+                  });
+              }
+            }}
+          />
+        </div>
+      </div>
+      <div className={"login-page-right"}>
+        <div className={"logo-container"}>
+          <GreenBudgetIcon />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/routes/LandingRoute.tsx
+++ src/components/routes/LandingRoute.tsx
@@ -1,20 +1,13 @@
-import React from "react";
 import { Route } from "react-router-dom";
-import { ShowHide } from "components/display";
-import { Footer } from "components/layout";
 
 interface LandingRouteProps {
-  footer?: boolean;
   [key: string]: any;
 }
 
-const LandingRoute = ({ footer = true, ...props }: LandingRouteProps): JSX.Element => {
+const LandingRoute = ({ ...props }: LandingRouteProps): JSX.Element => {
   return (
     <div className={"landing-content"}>
       <Route {...props} />
-      <ShowHide show={footer}>
-        <Footer copyright brand={false} />
-      </ShowHide>
     </div>
   );
 };
