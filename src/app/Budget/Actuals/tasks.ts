92ffb6b9e2f144b8c30ddd45ecfe623a31925bcb
nickmflorin <nickmflorin@gmail.com>
2021-03-28 18:14:28 -0400

    Uncommitted bug fix

--- src/app/Budget/Actuals/tasks.ts
+++ src/app/Budget/Actuals/tasks.ts
@@ -143,10 +143,8 @@ export function* handleActualUpdateTask(action: Redux.IAction<Table.RowChange>):
       // changed via the dropdown, so we need to udpate the row in the data used to populate the table.
       // We could do this by updating with a payload generated from the response, but it is quicker
       // to do it before hand.
-      // TODO: We should get rid of the preRequestPayload and just update preemtively with
-      // the entire row.
-      const preResponsePayload = ActualMapping.preRequestModelPayload(action.payload);
-      yield put(updateActualInStateAction({ ...model, ...preResponsePayload }));
+      const updatedModel = ActualMapping.newModelWithChanges(model, action.payload);
+      yield put(updateActualInStateAction(updatedModel));
 
       yield put(updatingActualAction({ id: model.id, value: true }));
       const requestPayload = ActualMapping.patchPayload(action.payload);
