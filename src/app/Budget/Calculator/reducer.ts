4ec85bc9ee44e26590a9a5ee8b81fde9b0768184
nickmflorin <nickmflorin@gmail.com>
2021-03-29 19:31:16 -0400

    Forgot to include these in the save

--- src/app/Budget/Calculator/reducer.ts
+++ src/app/Budget/Calculator/reducer.ts
@@ -1,11 +1,11 @@
 import { combineReducers } from "redux";
 
 import accountRootReducer from "./Account/reducer";
-import budgetRootReducer from "./Budget/reducer";
+import accountsRootReducer from "./Accounts/reducer";
 import subAccountRootReducer from "./SubAccount/reducer";
 
 const rootReducer = combineReducers({
-  budget: budgetRootReducer,
+  budget: accountsRootReducer,
   account: accountRootReducer,
   subaccount: subAccountRootReducer
 });
--- src/app/Budget/Calculator/sagas.ts
+++ src/app/Budget/Calculator/sagas.ts
@@ -2,7 +2,7 @@ import { SagaIterator } from "redux-saga";
 import { spawn } from "redux-saga/effects";
 
 import accountSaga from "./Account/sagas";
-import budgetSaga from "./Budget/sagas";
+import budgetSaga from "./Accounts/sagas";
 import subAccountSaga from "./SubAccount/sagas";
 
 export default function* rootSaga(): SagaIterator {
