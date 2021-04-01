e6d9562cfb9b859347c92cd840208b2ee936ce80
nickmflorin <nickmflorin@gmail.com>
2021-04-01 13:50:51 -0400

    Toggle view when ancestor select changes

--- src/components/layout/AncestorsBreadCrumbs.tsx
+++ src/components/layout/AncestorsBreadCrumbs.tsx
@@ -1,6 +1,6 @@
-import { useMemo } from "react";
+import React, { useCallback } from "react";
 import { useHistory } from "react-router-dom";
-import { map, isNil } from "lodash";
+import { map, isNil, find } from "lodash";
 import classNames from "classnames";
 
 import { Select } from "antd";
@@ -14,21 +14,39 @@ interface AncestorBreadCrumbItemProps {
 }
 
 const AncestorBreadCrumbItem = ({ budgetId, ancestor, last }: AncestorBreadCrumbItemProps): JSX.Element => {
-  /* eslint-disable indent */
-  const URL = useMemo(() => {
-    return ancestor.type === "subaccount"
-      ? `/budgets/${budgetId}/subaccounts/${ancestor.id}`
-      : ancestor.type === "account"
-      ? `/budgets/${budgetId}/accounts/${ancestor.id}`
-      : `/budgets/${budgetId}/accounts`;
-  }, [ancestor, budgetId]);
-
   const history = useHistory();
+
+  /* eslint-disable indent */
+  const getUrl = useCallback(
+    (entity: IAncestor | IBudgetItem): string => {
+      return entity.type === "subaccount"
+        ? `/budgets/${budgetId}/subaccounts/${entity.id}`
+        : entity.type === "account"
+        ? `/budgets/${budgetId}/accounts/${entity.id}`
+        : `/budgets/${budgetId}/accounts`;
+    },
+    [budgetId]
+  );
+
   if (!isNil(ancestor.siblings)) {
     return (
       <div className={classNames("ancestors-bread-crumb-item", { last })}>
         <div className={"select-wrapper"}>
-          <Select className={"select--ancestor"} value={ancestor.id} bordered={false}>
+          <Select
+            className={"select--ancestor"}
+            value={ancestor.id}
+            bordered={false}
+            onChange={(value: number) => {
+              const sibling = find(ancestor.siblings, { id: value });
+              if (isNil(sibling)) {
+                /* eslint-disable no-console */
+                console.error(`The select value corresponds to a sibling ${value} that is not in state!`);
+              } else if (sibling.id !== ancestor.id) {
+                console.log(getUrl(sibling));
+                history.push(getUrl(sibling));
+              }
+            }}
+          >
             {map(ancestor.siblings, (sibling: IBudgetItem) => {
               return (
                 <Select.Option value={sibling.id}>
@@ -41,16 +59,18 @@ const AncestorBreadCrumbItem = ({ budgetId, ancestor, last }: AncestorBreadCrumb
             })}
           </Select>
         </div>
-        {last === false && <span className={"slash"}>{"/"}</span>}
       </div>
     );
   }
   return (
-    <div className={classNames("ancestors-bread-crumb-item", { last })} onClick={() => history.push(URL)}>
-      <div className={"content-wrapper"}>
-        {ancestor.identifier}
-        {last === false && <span className={"slash"}>{"/"}</span>}
-      </div>
+    <div
+      className={classNames("ancestors-bread-crumb-item", {
+        last,
+        "budget-ancestor-bread-crumb-item": ancestor.type === "budget"
+      })}
+      onClick={() => history.push(getUrl(ancestor))}
+    >
+      <div className={"text-wrapper"}>{ancestor.identifier}</div>
     </div>
   );
 };
@@ -65,13 +85,15 @@ const AncestorsBreadCrumbs = ({ ancestors, budgetId }: AncestorsBreadCrumbsProps
     <div className={"ancestors-bread-crumbs"}>
       {map(ancestors, (ancestor: IAncestor, index: number) => {
         return (
-          /* eslint-disable indent */
-          <AncestorBreadCrumbItem
-            key={index}
-            ancestor={ancestor}
-            budgetId={budgetId}
-            last={index === ancestors.length - 1}
-          />
+          <React.Fragment>
+            <AncestorBreadCrumbItem
+              key={index}
+              ancestor={ancestor}
+              budgetId={budgetId}
+              last={index === ancestors.length - 1}
+            />
+            {index !== ancestors.length - 1 && <span className={"slash"}>{"/"}</span>}
+          </React.Fragment>
         );
       })}
     </div>
