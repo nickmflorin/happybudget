d563fb05422fed0d3dd742f8ac2bbe0e6fadf0f1
nickmflorin <nickmflorin@gmail.com>
2021-04-26 17:57:17 -0400

    Remove console.log statements

--- src/lib/util/forms.ts
+++ src/lib/util/forms.ts
@@ -2,9 +2,7 @@ export const payloadToFormData = <T extends { [key: string]: any } = { [key: str
   const formData = new FormData();
   Object.keys(data).forEach((key: string) => {
     const value: any | undefined = data[key as keyof T];
-    console.log(value);
     if (value !== undefined) {
-      console.log(`Adding ${key}: ${value} to form data`);
       formData.append(key, value);
     }
   });
