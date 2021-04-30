e046d8164f2d55247ef3dc4cd7344615532793de
nickmflorin <nickmflorin@gmail.com>
2021-04-29 22:41:59 -0400

    Add password requirements to signup

--- src/app/Signup/SignupForm.tsx
+++ src/app/Signup/SignupForm.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import classNames from "classnames";
 
-import { Form, Button, Input } from "antd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faQuestionCircle } from "@fortawesome/pro-solid-svg-icons";
+
+import { Button, Input, Tooltip } from "antd";
 import { MailOutlined, LockOutlined, UserOutlined } from "@ant-design/icons";
 
+import { Form, VerticalFlexCenter } from "components";
 import { RouterLink } from "components/links";
 import { validateEmail, validatePassword } from "lib/util/validate";
 import SocialButton from "../SocialButton";
@@ -35,12 +39,13 @@ const SignupForm = ({
   onGoogleError
 }: SignupFormProps): JSX.Element => {
   return (
-    <Form
+    <Form.Form
       style={style}
       form={form}
       className={classNames("signup-form", className)}
       onFinish={(values: ISignupFormValues) => onSubmit(values)}
     >
+      <div className={"input-group-title"}>{"Profile"}</div>
       <Form.Item name={"first_name"} rules={[{ required: true, message: "Please enter a valid first name." }]}>
         <Input className={"input"} size={"large"} prefix={<UserOutlined />} placeholder={"First name"} />
       </Form.Item>
@@ -63,6 +68,19 @@ const SignupForm = ({
       >
         <Input className={"input"} size={"large"} placeholder={"Email"} prefix={<MailOutlined />} />
       </Form.Item>
+      <div className={"input-group-title"} style={{ marginTop: 20 }}>
+        {"Password"}
+        <VerticalFlexCenter className={"question-icon-container"}>
+          <Tooltip
+            overlayStyle={{ whiteSpace: "pre-line" }}
+            title={
+              "Password must satisfy the following requirements:\n(1) Must contain at least 1 special character (!@#$%^&*).\n(2) Must container at least 1 uppercase letter.\n(3) Must contain at least 1 lowercase letter.\n(4) Must container at least one number.\n(5) Must be at least 8 characters long."
+            }
+          >
+            <FontAwesomeIcon className={"icon"} icon={faQuestionCircle} />
+          </Tooltip>
+        </VerticalFlexCenter>
+      </div>
       <Form.Item
         name={"password"}
         rules={[
@@ -95,24 +113,26 @@ const SignupForm = ({
       >
         <Input.Password className={"input"} size={"large"} placeholder={"Confirm"} prefix={<LockOutlined />} />
       </Form.Item>
-      <Button loading={loading} className={"btn--login"} htmlType={"submit"}>
-        {"Register"}
-      </Button>
-      <SocialButton
-        text={"Signup with Google"}
-        provider={"google"}
-        onGoogleSuccess={onGoogleSuccess}
-        onGoogleError={onGoogleError}
-      />
-      <div className={"login-text"}>
-        {"Already have an account?"}
-        <span>
-          <RouterLink to={"/login"} className={"login-link"}>
-            {"Log in"}
-          </RouterLink>
-        </span>
-      </div>
-    </Form>
+      <Form.Footer style={{ marginTop: 20 }}>
+        <Button loading={loading} className={"btn--login"} htmlType={"submit"}>
+          {"Register"}
+        </Button>
+        <SocialButton
+          text={"Signup with Google"}
+          provider={"google"}
+          onGoogleSuccess={onGoogleSuccess}
+          onGoogleError={onGoogleError}
+        />
+        <div className={"login-text"}>
+          {"Already have an account?"}
+          <span>
+            <RouterLink to={"/login"} className={"login-link"}>
+              {"Log in"}
+            </RouterLink>
+          </span>
+        </div>
+      </Form.Footer>
+    </Form.Form>
   );
 };
 
