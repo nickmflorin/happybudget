0e8353eb557fc1dd6461b51629858ff746854b13
nickmflorin <nickmflorin@gmail.com>
2021-04-30 13:15:35 -0400

    Dont lazy load in dash

---
+++ src/app/Dashboard/components/index.ts
@@ -0,0 +1,3 @@
+export { default as Templates } from "./Templates";
+export { default as Contacts } from "./Contacts";
+export { default as Budgets } from "./Budgets";
--- src/app/Dashboard/index.tsx
+++ src/app/Dashboard/index.tsx
@@ -1,16 +1,11 @@
-import React from "react";
 import { Switch, useHistory, useLocation } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCopy, faFilePlus, faAddressBook } from "@fortawesome/pro-light-svg-icons";
 
-import { componentLoader } from "lib/operational";
 import { Layout } from "components/layout";
 import { PrivateRoute } from "components/routes";
-
-const Contacts = React.lazy(() => componentLoader(() => import("./components/Contacts")));
-const Templates = React.lazy(() => componentLoader(() => import("./components/Templates")));
-const Budgets = React.lazy(() => componentLoader(() => import("./components/Budgets")));
+import { Contacts, Templates, Budgets } from "./components";
 
 const Dashboard = (): JSX.Element => {
   const history = useHistory();
