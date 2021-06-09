65937b58353976ca8f9b8d9d011838d6b9096583
nickmflorin <nickmflorin@gmail.com>
2021-06-08 21:27:20 -0400

    Fix bug

--- src/app/Budgeting/components/BudgetTable/cells/ModelTagCells/ModelTagCell.tsx
+++ src/app/Budgeting/components/BudgetTable/cells/ModelTagCells/ModelTagCell.tsx
@@ -3,19 +3,19 @@ import { Tag } from "components/tagging";
 import Cell, { StandardCellProps } from "../Cell";
 
 export interface ModelTagCellProps<M extends Model.Model, R extends Table.Row> extends StandardCellProps<R> {
-  readonly value: M | null;
+  readonly children: M | null;
   readonly leftAlign?: boolean;
 }
 
 const ModelTagCell = <M extends Model.Model, R extends Table.Row>({
-  value,
+  children,
   leftAlign,
   ...props
 }: ModelTagCellProps<M, R>): JSX.Element => {
   return (
-    <Cell {...props} onClear={() => props.setValue(null)} hideClear={value === null}>
+    <Cell {...props} onClear={() => props.setValue(null)} hideClear={children === null}>
       <div style={{ display: "flex", justifyContent: leftAlign === true ? "left" : "center" }}>
-        {!isNil(value) ? <Tag model={value} /> : <Tag.Empty visible={false} />}
+        {!isNil(children) ? <Tag model={children} /> : <Tag.Empty visible={false} />}
       </div>
     </Cell>
   );
