1f47a423842afc371ffc61a6f36b2b3932a22790
Seth Hall <1033332+sethburtonhall@users.noreply.github.com>
2021-07-27 11:43:48 -0400

    adds margin bottom to pdf header
    adds fade in/out to PDF preview
    
    rendering revisions, adjusts margin on pdf
    
    Update RenderDocument.tsx

--- src/components/pdf/primitive/RenderDocument.tsx
+++ src/components/pdf/primitive/RenderDocument.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 import { pdfjs, Document as ReactPdfDoc } from "react-pdf/dist/esm/entry.webpack";
 
 import { WrappedSpinner, RenderOrSpinner } from "components";
@@ -6,15 +8,25 @@ import { isNil } from "lodash";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const RenderDocument = (props: Pdf.RenderDocumentProps): JSX.Element => {
+  const [isRendering, setIsRendering] = useState(false);
+
+  useEffect(() => {
+    setIsRendering(!isNil(props.file) ? true : false);
+  }, [props.file]);
+
   return (
     <RenderOrSpinner loading={isNil(props.file) && props.loadingOnNoFile === true}>
       {!isNil(props.file) && (
-        <ReactPdfDoc
-          loading={<WrappedSpinner />}
-          {...props}
-          file={props.file}
-          options={{ workerSrc: "/pdf.worker.js" }}
-        />
+        <div className={!isRendering ? "fade-in-animation" : "fade-out-animation"}>
+          <ReactPdfDoc
+            loading={<WrappedSpinner />}
+            {...props}
+            file={props.file}
+            options={{ workerSrc: "/pdf.worker.js" }}
+            onLoadSuccess={() => setIsRendering(false)}
+            onLoadError={() => setIsRendering(false)}
+          />
+        </div>
       )}
     </RenderOrSpinner>
   );
