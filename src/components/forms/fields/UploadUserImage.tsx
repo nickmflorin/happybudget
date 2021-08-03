661e5d49ab737c064e604cfae55b091132dd0b6f
nickmflorin <nickmflorin@gmail.com>
2021-08-02 21:46:18 -0400

    Slight adjustment to profile image uploader

--- src/components/forms/fields/UploadUserImage.tsx
+++ src/components/forms/fields/UploadUserImage.tsx
@@ -1,5 +1,6 @@
 import classNames from "classnames";
 import Uploader, { UploaderProps } from "./Uploader";
+import "./UploadUserImage.scss";
 
 const UploadUserImage = (props: UploaderProps): JSX.Element => {
   return <Uploader {...props} className={classNames("user-image-uploader", props.className)} />;
