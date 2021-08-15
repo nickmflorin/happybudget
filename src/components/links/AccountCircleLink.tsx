16f4fefd5081af23d07abfb92a8a176470e86d2a
nickmflorin <nickmflorin@gmail.com>
2021-08-15 15:32:29 -0400

    Fix bug with header

--- src/components/links/AccountCircleLink.tsx
+++ src/components/links/AccountCircleLink.tsx
@@ -6,9 +6,16 @@ import { UserImageOrInitialsProps } from "components/layout/Layout/images/UserIm
 
 import "./AccountCircleLink.scss";
 
-const AccountCircleLink = ({ className, style, ...props }: UserImageOrInitialsProps): JSX.Element => {
+const AccountCircleLink = ({
+  className,
+  style,
+  onClick,
+  ...props
+}: UserImageOrInitialsProps & { readonly onClick?: () => void }): JSX.Element => {
+  // Note: We have to expose the onClick prop in order for this component to be used
+  // inside of an AntD dropdown.
   return (
-    <Link to={"#"} className={classNames("account-circle-link", className)} style={style}>
+    <Link to={"#"} className={classNames("account-circle-link", className)} style={style} onClick={onClick}>
       <UserImageOrInitials {...props} />
     </Link>
   );
