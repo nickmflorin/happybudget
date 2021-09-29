07d2f25ee23a5be8d10a92a4e6ea0f761a7a8b0f
nickmflorin <nickmflorin@gmail.com>
2021-09-29 11:13:49 -0400

    Fix bug with group edit button not showing up

--- src/components/tabling/generic/framework/cells/GroupCell.tsx
+++ src/components/tabling/generic/framework/cells/GroupCell.tsx
@@ -1,9 +1,7 @@
 import { useMemo } from "react";
-import { useSelector } from "react-redux";
-import { isNil, find, filter } from "lodash";
-import { createSelector } from "reselect";
+import { isNil } from "lodash";
 
-import { hooks, tabling } from "lib";
+import { tabling } from "lib";
 
 import { IconButton } from "components/buttons";
 import { Cell, ValueCell } from "components/tabling/generic/framework/cells";
@@ -26,33 +24,20 @@ const GroupCell = <
   ...props
 }: GroupCellProps<R, M, S>): JSX.Element => {
   const row: Table.Row<R, M> = props.node.data;
-  const groupRowSelector = createSelector(
-    [(state: Application.Store) => props.selector(state).data],
-    (data: Table.Row<R, M>[]) =>
-      filter(data, (r: Table.Row<R, M>) => tabling.typeguards.isGroupRow(r)) as Table.GroupRow<R>[]
-  );
-  const groupRows = useSelector(groupRowSelector);
-
-  const groupRow = useMemo<Table.GroupRow<R> | null>((): Table.GroupRow<R> | null => {
-    const groupId = tabling.typeguards.isGroupRow(row) ? tabling.rows.groupId(row.id) : null;
-    return isNil(groupId)
-      ? null
-      : (find(groupRows, { group: groupId } as any) as Table.GroupRow<R> | undefined) || null;
-  }, [row, hooks.useDeepEqualMemo(groupRows)]);
 
   const colorDef = useMemo<Table.RowColorDef>(() => {
     return props.getRowColorDef(row);
   }, [row]);
 
-  return !isNil(groupRow) ? (
+  return tabling.typeguards.isGroupRow(row) ? (
     <Cell {...props}>
       <div style={{ display: "flex" }}>
-        <span>{`${groupRow.groupData.name} (${groupRow.children.length} Line Items)`}</span>
+        <span>{`${row.groupData.name} (${row.children.length} Line Items)`}</span>
         <IconButton
           className={"btn--edit-group"}
           size={"xxsmall"}
           icon={"edit"}
-          onClick={() => onEdit?.(groupRow)}
+          onClick={() => onEdit?.(row)}
           style={!isNil(colorDef.color) ? { color: colorDef.color } : {}}
         />
       </div>
