538a5fc9ced748b7161cf1f316082c8b1d89a5cd
nickmflorin <nickmflorin@gmail.com>
2021-09-29 11:26:12 -0400

    Temporarily remove react strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import configureApplication from "./config";
 
-import React from "react";
 import ReactDOM from "react-dom";
 
 import "style/index.scss";
@@ -8,9 +7,25 @@ import App from "./app";
 
 configureApplication();
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+/*
+Note:
+
+At some point, we need to wrap <App /> in <React.StrictMode>.
+
+<React.StrictMode> serves the following purposes:
+
+1. Identifying components with unsafe lifecycles
+2. Warning about legacy string ref API usage
+3. Warning about deprecated findDOMNode usage
+4. Detecting unexpected side effects
+5. Detecting legacy context API
+
+However, the warnings about deprecated findDOMNode usage are coming up quite
+often due to external third-party packages not being completely React 17
+compliant yet.  In order to avoid these unnecessary and frequent warnings,
+we temporarily remove <React.StrictMode> until these third-party packages
+have a chance to address these warnings.
+
+The most frequent package that causes the warnings is AntD.
+*/
+ReactDOM.render(<App />, document.getElementById("root"));
