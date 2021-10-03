748b6a7cda2d525af7317fdbc647df708e71e7a9
nickmflorin <nickmflorin@gmail.com>
2021-10-03 18:42:26 -0400

    Fix null value bug

--- src/components/tabling/tables/ContactsTable/framework/cells/ContactNameCell.tsx
+++ src/components/tabling/tables/ContactsTable/framework/cells/ContactNameCell.tsx
@@ -10,22 +10,25 @@ interface ContactNameCellProps
 }
 
 const ContactNameCell = ({ value, ...props }: ContactNameCellProps): JSX.Element => {
-  return (
-    <Cell {...props}>
-      <div style={{ display: "flex", justifyContent: "left", alignItems: "center" }}>
-        <UserImageOrInitials
-          circle={true}
-          src={!isNil(value.image) ? value.image.url : null}
-          firstName={value.first_name}
-          lastName={value.last_name}
-          initialsStyle={{ width: 28, height: 28, marginRight: 8 }}
-          imageProps={{ wrapperStyle: { width: 28, height: 28, marginRight: 8 } }}
-          hideOnNoInitials={true}
-        />
-        {util.conditionalJoinString(value.first_name, value.last_name)}
-      </div>
-    </Cell>
-  );
+  if (!isNil(value)) {
+    return (
+      <Cell {...props}>
+        <div style={{ display: "flex", justifyContent: "left", alignItems: "center" }}>
+          <UserImageOrInitials
+            circle={true}
+            src={!isNil(value.image) ? value.image.url : null}
+            firstName={value.first_name}
+            lastName={value.last_name}
+            initialsStyle={{ width: 28, height: 28, marginRight: 8 }}
+            imageProps={{ wrapperStyle: { width: 28, height: 28, marginRight: 8 } }}
+            hideOnNoInitials={true}
+          />
+          {util.conditionalJoinString(value.first_name, value.last_name)}
+        </div>
+      </Cell>
+    );
+  }
+  return <span></span>;
 };
 
 export default ContactNameCell;
