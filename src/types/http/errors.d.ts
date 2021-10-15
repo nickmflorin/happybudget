c0e013121ec2e06bad5c69f2aa4e4e6abfa796aa
nickmflorin <nickmflorin@gmail.com>
2021-10-14 21:14:33 -0400

    Update error typings

--- src/types/http/errors.d.ts
+++ src/types/http/errors.d.ts
@@ -3,12 +3,9 @@
 namespace Http {
   type ErrorType = "unknown" | "http" | "field" | "global" | "auth";
 
-  type FieldErrorCode = "unique" | "invalid" | "required";
+  type FieldErrorCode = "unique" | "invalid" | "required" | "email_not_verified" | "email_does_not_exist" | "invalid_credentials";
   type AuthErrorCode =
     | "account_disabled"
-    | "invalid_credentials"
-    | "email_does_not_exist"
-    | "email_not_verified"
     | "invalid_social_token"
     | "invalid_social_provider";
   type HttpErrorCode = "not_found";
@@ -26,10 +23,12 @@ namespace Http {
     readonly message: string;
   }
 
+  /* eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars */
   interface IHttpNetworkError extends IBaseError {
     readonly url?: string;
   }
 
+  /* eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars */
   interface IHttpServerError extends IBaseError {
     readonly status: number;
     readonly url?: string;
@@ -42,6 +41,7 @@ namespace Http {
     readonly errors: E[];
   }
 
+  /* eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars */
   interface IHttpAuthenticationError extends IHttpClientError<AuthError> {
     readonly forceLogout?: boolean;
   }
@@ -56,8 +56,10 @@ namespace Http {
     readonly force_logout?: boolean;
   };
 
+  /* eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars */
   type Error = HttpError | UnknownError | FieldError | GlobalError | AuthError;
 
+  /* eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars */
   type ErrorResponse = {
     errors: Http.Error[];
     [key: string]: any;
