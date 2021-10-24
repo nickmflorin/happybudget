72e6643bb0782a554a783ba1413c4bcd8a33ebbf
nickmflorin <nickmflorin@gmail.com>
2021-10-24 14:15:45 -0400

    Putting footer in correct location

--- src/components/layout/Page/index.tsx
+++ src/components/layout/Page/index.tsx
@@ -1,6 +1,6 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useMemo } from "react";
 import classNames from "classnames";
-import { isNil } from "lodash";
+import { isNil, find, map, filter } from "lodash";
 import { WrapInApplicationSpinner } from "components";
 
 import PageHeader from "./PageHeader";
@@ -13,7 +13,7 @@ interface PageProps extends StandardComponentProps {
   // Required for pages that do not have a full page table in them to get the
   // content area scrollable.
   readonly contentScrollable?: boolean;
-  readonly children?: ReactNode;
+  readonly children?: SingleOrArray<ReactNode>;
   readonly loading?: boolean;
   readonly style?: React.CSSProperties;
   readonly title?: string;
@@ -22,23 +22,41 @@ interface PageProps extends StandardComponentProps {
   readonly extra?: JSX.Element[];
 }
 
-const Page = (props: PageProps): JSX.Element => (
-  <div className={"page"} style={props.style}>
-    {!isNil(props.title) && (
-      <PageHeader title={props.title} extra={props.extra}>
-        {props.subTitle}
-      </PageHeader>
-    )}
-    <WrapInApplicationSpinner loading={props.loading}>
-      {!isNil(props.children) && (
-        <div className={classNames("page-content", props.className, { scrollable: props.contentScrollable })}>
-          {props.children}
-        </div>
+const Page = (props: PageProps): JSX.Element => {
+  const footer = useMemo<JSX.Element | undefined>(() => {
+    return find(
+      Array.isArray(props.children) ? props.children : [props.children],
+      (child: JSX.Element) => child.type === PageFooter
+    ) as JSX.Element | undefined;
+  }, [props.children]);
+
+  const childrenArray = useMemo<JSX.Element[]>(() => {
+    return filter(
+      Array.isArray(props.children) ? props.children : [props.children],
+      (child: JSX.Element) => child.type !== PageFooter
+    ) as JSX.Element[];
+  }, [props.children]);
+
+  return (
+    <div className={"page"} style={props.style}>
+      {!isNil(props.title) && (
+        <PageHeader title={props.title} extra={props.extra}>
+          {props.subTitle}
+        </PageHeader>
       )}
-    </WrapInApplicationSpinner>
-    {!isNil(props.footer) && <PageFooter>{props.footer}</PageFooter>}
-  </div>
-);
+      <WrapInApplicationSpinner loading={props.loading}>
+        {childrenArray.length !== 0 && (
+          <div className={classNames("page-content", props.className, { scrollable: props.contentScrollable })}>
+            {map(childrenArray, (element: JSX.Element, index: number) => (
+              <React.Fragment key={index}>{element}</React.Fragment>
+            ))}
+          </div>
+        )}
+      </WrapInApplicationSpinner>
+      {!isNil(props.footer) ? <PageFooter>{props.footer}</PageFooter> : !isNil(footer) ? footer : <></>}
+    </div>
+  );
+};
 
 Page.Footer = PageFooter;
 Page.Header = PageHeader;
