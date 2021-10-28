e320e91b945bc0422d15b9a41e6281d5dffcabee
nickmflorin <nickmflorin@gmail.com>
2021-10-28 18:47:19 -0400

    Silence ag warning

--- src/components/tabling/generic/framework/editors/NullCellEditor.tsx
+++ src/components/tabling/generic/framework/editors/NullCellEditor.tsx
@@ -1,11 +1,27 @@
-import { forwardRef, ForwardedRef } from "react";
+import { forwardRef, useImperativeHandle } from "react";
+
+type DateEditorProps = Table.EditorParams<Tables.ActualRow, Model.Actual>;
 
 /**
  * Cell Editor used for cases where we do not want the cell to be directly
  * editable but still want the ability to copy and paste into the editor.
  */
-const NullCellEditor = (props: any, ref: ForwardedRef<any>) => {
-  return <></>;
+const NullCellEditor = (props: DateEditorProps, ref: any): JSX.Element => {
+  useImperativeHandle(ref, () => {
+    return {
+      getValue: () => {
+        return props.value;
+      },
+      isCancelBeforeStart() {
+        return true;
+      },
+      isCancelAfterEnd() {
+        return true;
+      }
+    };
+  });
+
+  return <div></div>;
 };
 
 export default forwardRef(NullCellEditor);
