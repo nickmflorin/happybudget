2198614560e0ba0f821e2aee1cbef74b307e0824
Seth Hall <1033332+sethburtonhall@users.noreply.github.com>
2021-11-01 10:24:43 -0400

    rearranges actuals column order to match mock

--- src/components/tabling/tables/ActualsTable/Columns.ts
+++ src/components/tabling/tables/ActualsTable/Columns.ts
@@ -6,33 +6,11 @@ type R = Tables.ActualRowData;
 type M = Model.Actual;
 
 const Columns: Table.Column<R, M>[] = [
-  tabling.columns.SelectColumn<R, M, Model.SimpleSubAccount | Model.SimpleMarkup | null>({
-    field: "owner",
-    headerName: "Sub-Account",
-    minWidth: 200,
-    width: 200,
-    getHttpValue: (
-      value: Model.SimpleSubAccount | Model.SimpleMarkup | null
-    ): Model.GenericHttpModel<"markup"> | Model.GenericHttpModel<"subaccount"> | null => {
-      if (!isNil(value)) {
-        return { id: value.id, type: value.type };
-      }
-      return value;
-    },
-    processCellForClipboard: (row: R) => {
-      if (!isNil(row.owner)) {
-        return row.owner.identifier || "";
-      }
-      return "";
-    },
-    cellRenderer: { data: "OwnerCell" },
-    cellEditor: "OwnerTreeEditor"
-  }),
   tabling.columns.BodyColumn<R, M>({
     field: "name",
     headerName: "Name",
     minWidth: 200,
-    flex: 3,
+    flex: 2,
     columnType: "longText"
   }),
   tabling.columns.SelectColumn({
@@ -43,15 +21,6 @@ const Columns: Table.Column<R, M>[] = [
     cellRenderer: { data: "ContactCell" },
     cellEditor: "ContactEditor",
     columnType: "contact"
-  }),
-  tabling.columns.BodyColumn<R, M>({
-    field: "purchase_order",
-    headerName: "PO",
-    width: 100,
-    minWidth: 100,
-    flex: 1,
-    columnType: "number",
-    tableColumnType: "body"
   }),
   tabling.columns.BodyColumn<R, M>({
     field: "date",
@@ -75,17 +44,6 @@ const Columns: Table.Column<R, M>[] = [
     minWidth: 140
   }),
   tabling.columns.BodyColumn<R, M>({
-    field: "payment_id",
-    headerName: "Pay ID",
-    width: 80,
-    minWidth: 80,
-    flex: 1,
-    columnType: "number"
-  }),
-  tabling.columns.AttachmentsColumn({
-    field: "attachments"
-  }),
-  tabling.columns.BodyColumn<R, M>({
     field: "value",
     headerName: "Amount",
     width: 100,
@@ -95,6 +53,48 @@ const Columns: Table.Column<R, M>[] = [
     valueSetter: tabling.valueSetters.floatValueSetter<R>("value"),
     cellRenderer: "BodyCell",
     columnType: "currency"
+  }),
+  tabling.columns.SelectColumn<R, M, Model.SimpleSubAccount | Model.SimpleMarkup | null>({
+    field: "owner",
+    headerName: "Sub-Account",
+    minWidth: 200,
+    width: 200,
+    getHttpValue: (
+      value: Model.SimpleSubAccount | Model.SimpleMarkup | null
+    ): Model.GenericHttpModel<"markup"> | Model.GenericHttpModel<"subaccount"> | null => {
+      if (!isNil(value)) {
+        return { id: value.id, type: value.type };
+      }
+      return value;
+    },
+    processCellForClipboard: (row: R) => {
+      if (!isNil(row.owner)) {
+        return row.owner.identifier || "";
+      }
+      return "";
+    },
+    cellRenderer: { data: "OwnerCell" },
+    cellEditor: "OwnerTreeEditor"
+  }),
+  tabling.columns.AttachmentsColumn({
+    field: "attachments"
+  }),
+  tabling.columns.BodyColumn<R, M>({
+    field: "purchase_order",
+    headerName: "PO",
+    width: 100,
+    minWidth: 100,
+    flex: 1,
+    columnType: "number",
+    tableColumnType: "body"
+  }),
+  tabling.columns.BodyColumn<R, M>({
+    field: "payment_id",
+    headerName: "Pay ID",
+    width: 80,
+    minWidth: 80,
+    flex: 1,
+    columnType: "number"
   }),
   tabling.columns.BodyColumn<R, M>({
     field: "notes",
