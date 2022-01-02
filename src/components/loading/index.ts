195dca21fbea3cf798d60521557d59d30877d948
Nick Florin <nickmflorin@gmail.com>
2022-01-02 09:53:31 -0500

    Removing unused component

--- src/components/loading/index.ts
+++ src/components/loading/index.ts
@@ -7,4 +7,3 @@ export { default as SavingChanges } from "./SavingChanges";
 export { default as ApplicationSpinner } from "./ApplicationSpinner";
 export { default as ConnectedApplicationSpinner } from "./ConnectedApplicationSpinner";
 export { default as WrapInApplicationSpinner } from "./WrapInApplicationSpinner";
-export { default as WaitToRender } from "./WaitToRender";
