94f22597e40cb6caf34058fee40ab69e68341079
Nick Florin <nickmflorin@gmail.com>
2022-01-11 12:57:48 -0500

    Funneling errors through to billing page

--- src/app/Settings/Billing/Billing.tsx
+++ src/app/Settings/Billing/Billing.tsx
@@ -1,13 +1,60 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useLocation, useHistory } from "react-router-dom";
+import { isNil } from "lodash";
 
 import * as api from "api";
-import { notifications } from "lib";
+import { notifications, users } from "lib";
 
 import { ProductsManager } from "components/billing";
 
 const Billing = (): JSX.Element => {
   const [subscribing, setSubscribing] = useState(false);
   const [managing, setManaging] = useState(false);
+  const user = users.hooks.useLoggedInUser();
+  const history = useHistory();
+  const location = useLocation<{
+    readonly notification?: UINotificationData;
+    readonly error?: Error;
+    readonly sessionId: string;
+  }>();
+
+  useEffect(() => {
+    const handleError = (e: Error, sessionId: string) => {
+      /* If the user incidentally visits the checkout success page without
+			   having been redirected from Stripe, the backend will be aware of this
+				 and will respond with `checkout_session_inactive`.  In this case, we do
+				 not want to issue a warning indicating that the checkout session needs
+				 to be manually associated with the user. */
+      const isSuperficialError =
+        e instanceof api.ClientError && e.billingError !== null && e.billingError.code === "checkout_session_inactive";
+      if (!isSuperficialError) {
+        console.error(
+          `FATAL Error: Could not sync the checkout session with ID ${sessionId} for user ${user.id}.` +
+            "This either means that the user is subscribed to products in Stripe but has not " +
+            "been associated with that subscription in our database.  This needs to be done manually." +
+            `\nOriginal Error: ${e}`
+        );
+        notifications.ui.banner.notify({
+          level: "error",
+          message: "Checkout Error",
+          detail: "Please contact support before you can access the features you are entitled to."
+        });
+      }
+    };
+
+    const locationState = location.state;
+    if (!isNil(locationState)) {
+      /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+      const { state, ...statelessLocation } = location;
+      history.replace(statelessLocation);
+      if (!isNil(locationState.error)) {
+        const e = locationState.error;
+        handleError(e, locationState.sessionId);
+      } else if (!isNil(locationState.notification)) {
+        notifications.ui.banner.notify(locationState.notification);
+      }
+    }
+  }, [location.state]);
 
   return (
     <div style={{ maxWidth: 800 }}>
--- src/app/Settings/Billing/CheckoutSuccess.tsx
+++ src/app/Settings/Billing/CheckoutSuccess.tsx
@@ -4,7 +4,6 @@ import { useDispatch } from "react-redux";
 import { isNil } from "lodash";
 
 import * as api from "api";
-import { users } from "lib";
 import { actions } from "store";
 
 import { ApplicationSpinner } from "components";
@@ -13,12 +12,13 @@ type IRedirect = {
   readonly pathname: "/billing";
   readonly state?: {
     readonly notification?: UINotificationData;
+    readonly error?: Error;
+    readonly sessionId: string;
   };
 };
 
 const CheckoutSuccess = (): JSX.Element => {
   const [redirect, setRedirect] = useState<IRedirect | null>(null);
-  const user = users.hooks.useLoggedInUser();
   const dispatch = useDispatch();
   const location = useLocation();
 
@@ -41,39 +41,19 @@ const CheckoutSuccess = (): JSX.Element => {
         .syncCheckoutSession({ session_id: sessionId })
         .then((u: Model.User) => {
           dispatch(actions.authenticated.updateLoggedInUserAction(u));
-          setRedirect({ pathname: "/billing" });
-        })
-        .catch((e: Error) => {
-          console.error(e);
-          /* If the user incidentally visits this page without having been
-             redirected from Stripe, the backend will be aware of this and will
-             respond with `checkout_session_inactive`.  In this case, we do not
-             want to issue a warning indicating that the checkout session needs
-             to be manually associated with the user. */
-          const isSuperficialError =
-            e instanceof api.ClientError &&
-            e.billingError !== null &&
-            e.billingError.code === "checkout_session_inactive";
-          if (!isSuperficialError) {
-            console.error(
-              `FATAL Error: Could not sync the checkout session with ID ${sessionId} for user ${user.id}.` +
-                "This either means that the user is subscribed to products in Stripe but has not " +
-                "been associated with that subscription in our database.  This needs to be done manually."
-            );
-            setRedirect({
-              pathname: "/billing",
-              state: {
-                notification: {
-                  level: "error",
-                  message: "There was an error completing the checkout process.",
-                  detail: "Please contact support before you can access the features you are entitled to."
-                }
+          setRedirect({
+            pathname: "/billing",
+            state: {
+              sessionId,
+              notification: {
+                closable: true,
+                level: "success",
+                message: "Your checkout was successful.  Thank you."
               }
-            });
-          } else {
-            setRedirect({ pathname: "/billing" });
-          }
-        });
+            }
+          });
+        })
+        .catch((e: Error) => setRedirect({ pathname: "/billing", state: { sessionId, error: e } }));
     }
   }, [location.search]);
 
