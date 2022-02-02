6710c1be9d25bac4b7d601b3f95437e91c2cb2f5
Nick Florin <nickmflorin@gmail.com>
2022-02-02 12:33:56 -0500

    Add promo code to modal

--- src/components/modals/MultipleBudgetProductPermissionModal.tsx
+++ src/components/modals/MultipleBudgetProductPermissionModal.tsx
@@ -14,7 +14,15 @@ function MultipleBudgetProductPermissionModal(props: Omit<ModalProps, "title" | 
       className={classNames("multiple-budget-product-pemrission-modal", props.className)}
       title={"You've Reached the Free Tier Limit"}
     >
-      <p className={"modal-text"}>{"Not to worry! You can upgrade your plan here."}</p>
+      {/* It is not ideal that we have to hardcode in this promo code - but it is a
+			stop-gap until we integrate more stable trialing with Stripe. */}
+      <p className={"modal-text"}>
+        <span>{"Not to worry! You can upgrade your plan here.  Use promo code "}</span>
+        <span>
+          <b>{"green30"}</b>
+        </span>
+        <span>{" for a free 30 day trial."}</span>
+      </p>
       <div style={{ display: "flex", justifyContent: "center", flexDirection: "row", marginTop: 18 }}>
         <PrimaryButton
           onClick={() => {
