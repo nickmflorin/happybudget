4c73d5bc1d68ed05ec3b40c2d4b551f1108c064f
nickmflorin <nickmflorin@gmail.com>
2022-02-21 14:05:35 -0500

    fixing select styling

--- src/components/fields/PercentInput.tsx
+++ src/components/fields/PercentInput.tsx
@@ -3,7 +3,7 @@ import { Icon } from "components";
 import Input, { InputProps } from "./Input";
 
 const PercentInput = (props: InputProps): JSX.Element => {
-  return <Input size={"large"} {...props} suffix={<Icon icon={"percentage"} />} />;
+  return <Input {...props} suffix={<Icon icon={"percentage"} />} />;
 };
 
 export default React.memo(PercentInput);
