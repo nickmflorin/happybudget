73bf58c21f62c075852c878305fb15d424dbc3ad
Nick Florin <nickmflorin@gmail.com>
2022-02-26 13:30:08 -0500

    Expression tweaks

--- src/components/charts/ActualsByDateChart.tsx
+++ src/components/charts/ActualsByDateChart.tsx
@@ -12,44 +12,42 @@ interface BudgetTotalChartProps<D extends Charts.Datum = Charts.Datum> {
   readonly tooltipLabelPrefix?: (datum: BarTooltipProps<D>) => string;
 }
 
-const ActualsByDateChart = <D extends Charts.Datum = Charts.Datum>(props: BudgetTotalChartProps<D>): JSX.Element => {
-  return (
-    <ResponsiveBar<D>
-      data={props.data}
-      margin={{ top: 20, right: 10, bottom: 22, left: 40 }}
-      padding={0.8}
-      colors={{ datum: "data.color" }}
-      borderWidth={1}
-      borderRadius={6}
-      borderColor={{ from: "color" }}
-      axisLeft={{
-        tickValues: 6
-      }}
-      axisBottom={{
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: "country",
-        legendPosition: "middle",
-        legendOffset: 32
-      }}
-      tooltip={(params: BarTooltipProps<D>): JSX.Element => {
-        return !isNil(props.tooltip) ? (
-          props.tooltip(params)
-        ) : (
-          <Tooltip<BarTooltipProps<D>>
-            labelPrefix={props.tooltipLabelPrefix?.(params)}
-            label={params.indexValue}
-            value={params.value}
-            valueFormatter={(v: string | number) => tabling.formatters.currencyValueFormatter(v)}
-          />
-        );
-      }}
-      enableLabel={false}
-      minValue={0}
-      maxValue={1200}
-    />
-  );
-};
+const ActualsByDateChart = <D extends Charts.Datum = Charts.Datum>(props: BudgetTotalChartProps<D>): JSX.Element => (
+  <ResponsiveBar<D>
+    data={props.data}
+    margin={{ top: 20, right: 10, bottom: 22, left: 40 }}
+    padding={0.8}
+    colors={{ datum: "data.color" }}
+    borderWidth={1}
+    borderRadius={6}
+    borderColor={{ from: "color" }}
+    axisLeft={{
+      tickValues: 6
+    }}
+    axisBottom={{
+      tickSize: 5,
+      tickPadding: 5,
+      tickRotation: 0,
+      legend: "country",
+      legendPosition: "middle",
+      legendOffset: 32
+    }}
+    tooltip={(params: BarTooltipProps<D>): JSX.Element => {
+      return !isNil(props.tooltip) ? (
+        props.tooltip(params)
+      ) : (
+        <Tooltip<BarTooltipProps<D>>
+          labelPrefix={props.tooltipLabelPrefix?.(params)}
+          label={params.indexValue}
+          value={params.value}
+          valueFormatter={(v: string | number) => tabling.formatters.currencyValueFormatter(v)}
+        />
+      );
+    }}
+    enableLabel={false}
+    minValue={0}
+    maxValue={1200}
+  />
+);
 
 export default ActualsByDateChart;
--- src/components/charts/BudgetTotalChart.tsx
+++ src/components/charts/BudgetTotalChart.tsx
@@ -11,46 +11,44 @@ interface BudgetTotalChartProps<D extends Charts.Datum = Charts.Datum> {
   readonly tooltipLabelPrefix?: (datum: Charts.ComputedDatum<D>) => string;
 }
 
-const BudgetTotalChart = <D extends Charts.Datum = Charts.Datum>(props: BudgetTotalChartProps<D>): JSX.Element => {
-  return (
-    <ResponsivePie<D>
-      data={props.data}
-      margin={{ top: 10, right: 10, bottom: 10, left: 0 }}
-      innerRadius={0.92}
-      activeOuterRadiusOffset={8}
-      colors={{ datum: "data.color" }}
-      borderWidth={1}
-      borderColor={{ from: "color" }}
-      enableArcLinkLabels={false}
-      enableArcLabels={false}
-      valueFormat={"<$.2~s"}
-      legends={[
-        {
-          anchor: "left",
-          direction: "column",
-          justify: false,
-          translateX: 0,
-          translateY: 0,
-          itemWidth: 100,
-          itemHeight: 20,
-          itemsSpacing: 23,
-          symbolSize: 10,
-          itemDirection: "left-to-right"
-        }
-      ]}
-      tooltip={(params: { datum: Charts.ComputedDatum<D> }): JSX.Element =>
-        !isNil(props.tooltip) ? (
-          props.tooltip(params.datum)
-        ) : (
-          <Tooltip<Charts.ComputedDatum<D>>
-            labelPrefix={props.tooltipLabelPrefix?.(params.datum)}
-            datum={params.datum}
-            valueFormatter={(v: string | number) => tabling.formatters.currencyValueFormatter(v)}
-          />
-        )
+const BudgetTotalChart = <D extends Charts.Datum = Charts.Datum>(props: BudgetTotalChartProps<D>): JSX.Element => (
+  <ResponsivePie<D>
+    data={props.data}
+    margin={{ top: 10, right: 10, bottom: 10, left: 0 }}
+    innerRadius={0.92}
+    activeOuterRadiusOffset={8}
+    colors={{ datum: "data.color" }}
+    borderWidth={1}
+    borderColor={{ from: "color" }}
+    enableArcLinkLabels={false}
+    enableArcLabels={false}
+    valueFormat={"<$.2~s"}
+    legends={[
+      {
+        anchor: "left",
+        direction: "column",
+        justify: false,
+        translateX: 0,
+        translateY: 0,
+        itemWidth: 100,
+        itemHeight: 20,
+        itemsSpacing: 23,
+        symbolSize: 10,
+        itemDirection: "left-to-right"
       }
-    />
-  );
-};
+    ]}
+    tooltip={(params: { datum: Charts.ComputedDatum<D> }): JSX.Element =>
+      !isNil(props.tooltip) ? (
+        props.tooltip(params.datum)
+      ) : (
+        <Tooltip<Charts.ComputedDatum<D>>
+          labelPrefix={props.tooltipLabelPrefix?.(params.datum)}
+          datum={params.datum}
+          valueFormatter={(v: string | number) => tabling.formatters.currencyValueFormatter(v)}
+        />
+      )
+    }
+  />
+);
 
 export default BudgetTotalChart;
