f6570ab111458f87693a0c5c195bd062deb016c9
Nick Florin <nickmflorin@gmail.com>
2022-03-09 14:58:17 -0500

    Fixing bug with periods and downloading pdf files

--- src/lib/util/files.ts
+++ src/lib/util/files.ts
@@ -1,9 +1,9 @@
 import { isNil } from "lodash";
 import mime from "mime";
 
-export const fileSizeInMB = (file: File | number) => (typeof file === "number" ? file : file.size) / 1024 / 1024;
+export const fileNameCompat = (filename: string) => filename.replace(".", "");
 
-export const getFileType = (filename: string): string | undefined => filename.split(".").pop();
+export const fileSizeInMB = (file: File | number) => (typeof file === "number" ? file : file.size) / 1024 / 1024;
 
 export const fileSizeString = (file: File | number) => `${fileSizeInMB(file).toFixed(2)} MB`;
 
@@ -19,27 +19,94 @@ class InvalidFileExtensionError extends Error {
   }
 }
 
-export const parseFileNameAndExtension = (name: string, ext?: string): [string, string] => {
-  let extension: string;
-  let explicitExtension: string | null = null;
-  if (!isNil(ext)) {
-    if (ext.startsWith(".")) {
-      explicitExtension = ext.slice(1);
+export const getFileType = (filename: string, strict = false): string | null => {
+  const returnNull = () => {
+    if (strict === false) {
+      return null;
+    }
+    throw new InvalidFileNameError(filename);
+  };
+  if (filename.indexOf(".") !== -1) {
+    const ext = filename.split(".").pop();
+    if (isNil(ext)) {
+      return returnNull();
+    } else if (ext.trim() === "") {
+      throw new InvalidFileNameError(filename);
+    }
+    return ext.trim();
+  }
+  return returnNull();
+};
+
+export const removeExtensionFromFileName = (filename: string, ext?: string) => {
+  let extension: string | null = ext || null;
+  if (!isNil(extension) && filename.indexOf(extension) === -1) {
+    throw new InvalidFileExtensionError(extension);
+  } else if (isNil(extension)) {
+    extension = getFileType(filename, true);
+    if (isNil(extension)) {
+      throw new InvalidFileNameError(filename);
     }
   }
-  const extensionFromName = getFileType(name);
+  extension = extension.startsWith(".") ? extension : `.${extension}`;
+  return filename.slice(0, filename.lastIndexOf(extension));
+};
+
+export const formatFileNameForDownload = (filename: string, extParsed = false): string => {
+  if (!extParsed) {
+    const ext = getFileType(filename, false);
+    if (!isNil(ext)) {
+      return `${formatFileNameForDownload(removeExtensionFromFileName(filename, ext), true)}.${ext}`;
+    }
+    return formatFileNameForDownload(filename, true);
+  }
+  return filename.replace(".", "");
+};
+
+type FileWithExtension = `${string}.${string}`;
+
+type ParseFileNameOptions = {
+  readonly ext?: string;
+  readonly formatForDownload?: boolean;
+};
+
+type ParsedFileName = {
+  readonly name: string;
+  readonly extension: string;
+  readonly fullName: FileWithExtension;
+};
+
+export const parseFileName = (name: string, options?: ParseFileNameOptions): ParsedFileName => {
+  let extension: string;
+
+  const ext = !isNil(options?.ext)
+    ? options?.ext.startsWith(".")
+      ? options.ext.slice(1)
+      : options?.ext
+    : options?.ext;
+
+  const extensionFromName = getFileType(name, false);
   if (isNil(extensionFromName)) {
-    if (isNil(explicitExtension)) {
+    /* If the extension cannot be determined from the name and the extension is
+		   not explicitly provided, throw an error. */
+    if (isNil(ext)) {
       throw new InvalidFileNameError(name);
     }
-    extension = explicitExtension;
+    extension = ext;
+  } else if (!isNil(ext) && ext !== extensionFromName) {
+    /* If the extension from the name does not equal the explicitly provided
+       extension, the extension from the name is not the real extension and
+       there is an extra period in the filename. */
+    extension = ext;
   } else {
-    if (!isNil(explicitExtension) && explicitExtension !== extensionFromName) {
-      throw new InvalidFileExtensionError(explicitExtension);
-    }
     extension = extensionFromName;
+    name = removeExtensionFromFileName(name, extension);
   }
-  return [name, extension];
+  let fullName = `${name}.${extension}` as `${string}.${string}`;
+  if (options?.formatForDownload) {
+    fullName = formatFileNameForDownload(fullName) as FileWithExtension;
+  }
+  return { name: removeExtensionFromFileName(fullName, extension), extension, fullName };
 };
 
 export const getDataFromBlob = (file: File | Blob): Promise<string | ArrayBuffer> =>
@@ -63,11 +130,6 @@ export const getDataFromURL = (url: string): Promise<string | ArrayBuffer> =>
     xhr.responseType = "blob";
     xhr.send();
   });
-
-type DownloadOptions = {
-  readonly ext?: string;
-  readonly includeExtensionInName?: boolean;
-};
 
 export const downloadData = async (data: string, name: string) => {
   const link = document.createElement("a");
@@ -101,31 +163,27 @@ export const extensionIsImage = (ext: string) => {
 export const download = async (
   fileObj: string | ArrayBuffer | Blob,
   nm: string,
-  options: DownloadOptions = { includeExtensionInName: true }
+  options?: Omit<ParseFileNameOptions, "formatForDownload">
 ) => {
   /* eslint-disable-next-line prefer-const */
-  let [name, extension] = parseFileNameAndExtension(nm, options.ext);
-  if (!name.endsWith(extension) && options.includeExtensionInName === true) {
-    name = `${name}.${extension}`;
-  } else if (name.endsWith(extension) && options.includeExtensionInName === false) {
-    name = name.slice(0, name.indexOf(extension) - 1);
-  }
-  const mimeType = mime.getType(extension);
+  const parsed = parseFileName(nm, options);
+
+  const mimeType = mime.getType(parsed.extension);
   if (isNil(mimeType)) {
-    throw new InvalidFileExtensionError(extension);
+    throw new InvalidFileExtensionError(parsed.extension);
   }
   let blob: Blob;
   if (!(fileObj instanceof Blob)) {
     if (fileObj instanceof ArrayBuffer) {
       blob = new Blob([fileObj], { type: mimeType });
-      downloadBlob(blob, name);
+      downloadBlob(blob, parsed.fullName);
       return;
     } else {
-      downloadData(fileObj, name);
+      downloadData(fileObj, parsed.fullName);
       return;
     }
   } else {
-    downloadBlob(fileObj, name);
+    downloadBlob(fileObj, parsed.fullName);
   }
 };
 
@@ -151,13 +209,9 @@ export const downloadAsCsvFile = async (filename: string, data: CSVData) => {
   for (let i = 0; i < data.length; i++) {
     csvFile += processRow(data[i]);
   }
-
-  if (!filename.endsWith(".csv")) {
-    filename = `${filename}.csv`;
-  }
-
+  const parsed = parseFileName(filename, { ext: "csv" });
   const blob = new Blob([csvFile], { type: "text/csv;charset=utf-8;" });
-  downloadBlob(blob, filename);
+  downloadBlob(blob, parsed.fullName);
 };
 
 export const truncateFileName = (file: string, length: number) => {
